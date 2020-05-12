7e244c17b43b18bc182c9f27bd3c92705e2ce490
Lucas Beneston <benestonlucas@gmail.com>
2020-05-12 12:30:31 +0200

    Section des films sur le réseau netflix - NETFLIX ORIGINALS

--- components/Section.mjs
+++ components/Section.mjs
@@ -1,8 +1,8 @@
 export default function Section(props) {
-  return `
+    return `
         <div class="movies__container--movie__netflix">
             <img src="https://image.tmdb.org/t/p/original/${props.poster_path}"
-                class="movies__container--movie-image" />
+                class="movies__container--movie-image">
         </div>
-        `;
+          `;
 }
--- index.js
+++ index.js
@@ -38,15 +38,11 @@ import Section from "./components/Section.mjs";
   document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
 })();
 
+
 (async () => {
   let movie = await fetchNetflixOriginals();
-  let moviesNetflix = document.querySelectorAll(".movies__container--movie__netflix").innerHTML
-  moviesNetflix = Section(movie.results[0]);
-  let netflixRow = document.querySelector('#movies-row-netflix');
-  netflixRow.append(moviesNetflix);
-
-  console.log("Console.log de moviesNetlfix : ", moviesNetflix)
-  console.log("Console.log de netlfixRow : ", netflixRow)
-  console.log("Console.log de fetchNetflixOriginals : ", movie);
-  console.log("Console.log de Section(movie.results[0]) : ", Section(movie.results[0]));
+  for (let i = 1; i < movie.results.length; i++) {
+    let netflixRow = document.querySelector('#movies-row-netflix');
+    netflixRow.innerHTML += Section(movie.results[i]);
+  }
 })();
