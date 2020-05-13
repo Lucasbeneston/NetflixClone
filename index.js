d81559c64db6807e11501377cbeb2e664a61c7f5
Lucas Beneston <benestonlucas@gmail.com>
2020-05-13 19:36:40 +0200

    Changement let image

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ import Modal from './components/Modal.mjs';
 
     let divModalNetflix = document.getElementById('modal-netflix');
 
-    let imagesNetflix = document.getElementsByClassName('movies__container--movie__netflix');
+    let images = document.getElementsByClassName('movies__container--movie-image');
     
-    for (let i = 0; i < imagesNetflix.length; i++) {
-    imagesNetflix[i].addEventListener('click', async(event) => {
+    for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', async(event) => {
       let id = movies[i].id;
       let modalNetflix = await fetchModalNetflix(id);  
 
@@ -118,47 +118,13 @@ import Modal from './components/Modal.mjs';
 
 
 
-// (async () => {
-//   let movie = await fetchNetflixOriginals();
-
-//   // let idMovie = movie.results.find(id => id).id
-//   let idMovie = movie.results[4].id
-
-//   let movies = fetchModal(idMovie);
-
-//   document.addEventListener('click',()=> {
-  
-// // Séléctionner la div clické
- 
-// // Séléctioner l'Id de la div clické 
-    
-  
-//   console.log("Console.log de idMovie : ", idMovie)
-//   console.log("Console.log de movie : ", movie)
-//   console.log("Console.log de movies : ", movies)
-
-   
-//    for (let i = 0; i < movie.results.length; i++) {
-
-//      console.log(movie.results[i])
-//      console.log(movie.results)
-
-//      if (movie.results[i] === movie.results){
-//       document.getElementById("modal-netflix").innerHTML = Modal(movie.results[i]);
-//       document.getElementById("modal-netflix").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.results[i].backdrop_path})`;
-//      }
-//    }
-//      })
-// })();
 
 
 
 
-// (async () => {
-//   let movie = await fetchModal(13); // Comment utiliser l'id du film clické ?
-//   document.getElementById("modal-netflix").innerHTML = Modal(movie);
-//   document.getElementById("modal-netflix").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
-// })();
+
+
+
 
 
 
