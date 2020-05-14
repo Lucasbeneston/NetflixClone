74395377621540334250bbf855f1d8747c7bc718
Lucas Beneston <benestonlucas@gmail.com>
2020-05-14 14:26:49 +0200

    Image default

--- components/Section.mjs
+++ components/Section.mjs
@@ -8,56 +8,106 @@ export function SectionNetflix(props) {
 }
 
 export function SectionTrending(props) {
-    return `
+    if (props.backdrop_path !== null){
+        return `
         <div class="movies__container--movie__trending">
-            <img src="https://image.tmdb.org/t/p/w500/${props.backdrop_path || "/wooZWiC4NWH0ahCSUOogEmVejHo.jpg"}"
-                class="movies__container--movie-image">
+            <img
+                src="https://image.tmdb.org/t/p/w500/${props.backdrop_path}"
+                class="movies__container--movie-image"
+            />
         </div>
-          `;
+            `;
+    } else {
+        return `
+        <div class="movies__container--movie__trending">
+            <img
+                src="/img/default.jpg"
+                class="movies__container--movie-image"
+            />
+        </div>`
+    }
 }
 
 export function SectionTopRated(props) {
-    return `
-    <div class="movies__container--movie__toprated">
-        <img
-            src="https://image.tmdb.org/t/p/w500/${props.backdrop_path || "/wooZWiC4NWH0ahCSUOogEmVejHo.jpg"}"
-            class="movies__container--movie-image"
-        />
-    </div>
-        `;
+    if (props.backdrop_path == null){
+        return `
+        <div class="movies__container--movie__toprated">
+            <img
+                src="/img/default.jpg"
+                class="movies__container--movie-image"
+            />
+        </div>`
+    } else {
+        return `
+        <div class="movies__container--movie__toprated">
+            <img
+                src="https://image.tmdb.org/t/p/w500/${props.backdrop_path}"
+                class="movies__container--movie-image"
+            />
+        </div>
+            `;
+    }
 }
 
 export function SectionGenreAction(props) {
-    return `
-    <div class="movies__container--movie__genreAction">
-        <img
-            src="https://image.tmdb.org/t/p/w500/${props.backdrop_path || "/wooZWiC4NWH0ahCSUOogEmVejHo.jpg"}"
-            class="movies__container--movie-image"
-        />
-    </div>
-        `;
+    if (props.backdrop_path == null){
+        return `
+        <div class="movies__container--movie__genreAction">
+            <img
+                src="/img/default.jpg"
+                class="movies__container--movie-image"
+            />
+        </div>`
+    } else {
+        return `
+        <div class="movies__container--movie__genreAction">
+            <img
+                src="https://image.tmdb.org/t/p/w500/${props.backdrop_path}"
+                class="movies__container--movie-image"
+            />
+        </div>
+            `;
+    }
 }
 
 export function SectionGenreComedy(props) {
-    return `
-    <div class="movies__container--movie__genreComedy">
-        <img
-            src="https://image.tmdb.org/t/p/w500/${props.backdrop_path || "/wooZWiC4NWH0ahCSUOogEmVejHo.jpg"}"
-            class="movies__container--movie-image"
-        />
-    </div>
-        `;
+    if (props.backdrop_path == null){
+        return `
+        <div class="movies__container--movie__genreComedy">
+            <img
+                src="/img/default.jpg"
+                class="movies__container--movie-image"
+            />
+        </div>`
+    } else {
+        return `
+        <div class="movies__container--movie__genreComedy">
+            <img
+                src="https://image.tmdb.org/t/p/w500/${props.backdrop_path}"
+                class="movies__container--movie-image"
+            />
+        </div>
+            `;
+    }
 }
 
 export function SectionGenreDocumentary(props) {
-    return `
-    <div class="movies__container--movie__genreDocumentary">
-        <img
-            src="https://image.tmdb.org/t/p/w500/${props.backdrop_path || "/wooZWiC4NWH0ahCSUOogEmVejHo.jpg"}"
-            class="movies__container--movie-image"
-        />
-    </div>
-        `;
+    if (props.backdrop_path == null){
+        return `
+        <div class="movies__container--movie__genreDocumentary">
+            <img
+                src="/img/default.jpg"
+                class="movies__container--movie-image"
+            />
+        </div>`
+    } else {
+        return `
+        <div class="movies__container--movie__genreDocumentary">
+            <img
+                src="https://image.tmdb.org/t/p/w500/${props.backdrop_path}"
+                class="movies__container--movie-image"
+            />
+        </div>
+            `;
+    }
 }
-
-// "/img/default.jpg"
